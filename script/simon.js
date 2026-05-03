eceb382f2415bc6b73b2b646988e317b4f6d3f40
dilsontorrefiel <83708088+dilson-torrefiel@users.noreply.github.com>
2026-05-03 14:00:51 +0400

    Add files via upload

---
+++ script/simon.js
@@ -0,0 +1,115 @@
+/**
+ * Simon Game
+ * created by: Dilson Torrefiel
+ * 05 2026
+ */
+let memory = [];
+let player = [];
+let isMatch = false;
+let level = 0;
+$(".score")[0].innerText = "Level: " + level;
+
+/**
+ * Activate next pattern
+ * @returns computer next pattern
+ */
+function activateNext() {
+  const min = 0;
+  const max = 3;
+  const random = Math.floor(Math.random() * (max - min + 1) + min);
+  const pads = $(".pad");
+
+  if ($(pads[random]).hasClass("active") === false) {
+    $(pads[random]).addClass("active");
+  }
+  setTimeout(() => {
+    $(pads[random]).removeClass("active");
+  }, 600);
+  memory.push(random);
+  $(".start").addClass("hide");
+}
+
+/**
+ * Re start Game and reset data in memory
+ */
+function reStart() {
+  memory = [];
+  player = [];
+  level = 0;
+  activateNext();
+  $(".heading h2").addClass("hide");
+  $(".score")[0].innerText = "Level: " + level;
+  $(".heading h2").addClass("hide");
+}
+
+/**
+ * Event Listener to player's clicks
+ */
+$(".pad").on("click", function (event) {
+  player.push(Number(event.target.attributes[1].nodeValue));
+  animateZoom(event.target.id);
+  playSound(event.target.id);
+  levelUp(memory, player);
+  gameOver(memory, player);
+});
+
+/**
+ * Checks if human's memory is the same with the computer memory then level-up
+ * @param {computer memory} computer
+ * @param {player} human
+ */
+function levelUp(computer, human) {
+  isMatch = JSON.stringify(computer) === JSON.stringify(human);
+  if (isMatch && computer.length === human.length && computer.length !== 0) {
+    setTimeout(() => {
+      activateNext();
+    }, 500);
+    player = [];
+    level++;
+    $(".score")[0].innerText = "Level: " + level;
+  }
+}
+
+/**
+ * Check if both array patterns are the same else game over
+ * @param {memory} computer
+ * @param {player} human
+ */
+function gameOver(computer, human) {
+  if (computer.length === human.length) {
+    if (isMatch == false) {
+      $(".heading h2")[0].innerText = "Game Over!";
+
+      // Making it blink multiple times using a loop
+      for (let i = 0; i < 5; i++) {
+        $(".heading h2").fadeOut(200).fadeIn(200);
+      }
+      // unhide start button & change onClick event target
+      $(".start").removeClass("hide");
+      $(".start").attr("onClick", "reStart()");
+
+      // play sound
+      playSound("wrong");
+    }
+  }
+}
+
+/**
+ * Apply zoom event
+ * @param {event id} event
+ */
+function animateZoom(event) {
+  $("#" + event).addClass("zoom");
+  setTimeout(() => {
+    $("#" + event).removeClass("zoom");
+  }, 500);
+}
+
+/**
+ * Handle play sound
+ * @param {sound file name} sound
+ */
+function playSound(sound) {
+  var mp3 = new Audio("./sounds/" + sound + ".mp3");
+  mp3.play();
+}
